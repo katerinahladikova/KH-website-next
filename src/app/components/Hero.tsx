71602c269a74766802a5885dd27bd13ca082bf7b
katerinahladikova <kat.hladikova@email.cz>
2025-10-21 12:45:05 +0200

    hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,8 +3,12 @@ import { useTranslations } from "next-intl";
 export default function Hero() {
     const t = useTranslations("hero");
     return (
-        <div>
-            <h1>{t("header")}</h1>
+        <div className="container flex flex-col h-screen items-center justify-center uppercase font-bold w-min">
+            <h1 className="text-9xl tracking-[-5px] leading-[0.834] text-center">{t("header")}</h1>
+            <div className="justify-between w-full px-20 flex text-sm">
+                <p dangerouslySetInnerHTML={{ __html: t("subtitle") }}></p>
+                <p>({t("year")})</p>
+            </div>
         </div>
     );
 }
--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -8,37 +8,39 @@ export function Navigation() {
     const t = useTranslations("navigation");
     const pathname = usePathname();
     return (
-        <nav className="text-inky container pt-3.5 pb-2.5 flex gap-16 items-center">
-            <Link href="#" className="font-bold uppercase">
-                Kateřina Hladíková
-            </Link>
-            <div className="uppercase font-bold text-sm flex justify-between gap-10 items-center">
-                <Link
-                    href="#"
-                    className="relative before:absolute before:left-1/2 before:-bottom-2 before:h-0.75 before:bg-inky before:-translate-x-1/2 before:transition-[width] before:w-0 hover:before:w-6"
-                >
-                    {t("about")}
+        <div className="fixed top-0 left-0 right-0">
+            <nav className="text-inky container pt-3.5 pb-2.5 flex gap-16 items-center">
+                <Link href="#" className="font-bold uppercase">
+                    Kateřina Hladíková
                 </Link>
-                <Link
-                    href="#"
-                    className="relative before:absolute before:left-1/2 before:-bottom-2 before:h-0.75 before:bg-inky before:-translate-x-1/2 before:transition-[width] before:w-0 hover:before:w-6"
-                >
-                    {t("portfolio")}
-                </Link>
-            </div>
-            <div className="uppercase font-bold text-sm m-auto mr-0 flex justify-between gap-5 items-center">
-                <p>{t("contact")}</p>
-                <Mail mail="kat.hladikova@email.cz" />
-                <div className="flex justify-center gap-1 text-base">
-                    <Link href={pathname} locale="cs">
-                        CZ
+                <div className="uppercase font-bold text-sm flex justify-between gap-10 items-center">
+                    <Link
+                        href="#"
+                        className="relative before:absolute before:left-1/2 before:-bottom-2 before:h-0.75 before:bg-inky before:-translate-x-1/2 before:transition-[width] before:w-0 hover:before:w-6"
+                    >
+                        {t("about")}
                     </Link>
-                    |
-                    <Link href={pathname} locale="en">
-                        EN
+                    <Link
+                        href="#"
+                        className="relative before:absolute before:left-1/2 before:-bottom-2 before:h-0.75 before:bg-inky before:-translate-x-1/2 before:transition-[width] before:w-0 hover:before:w-6"
+                    >
+                        {t("portfolio")}
                     </Link>
                 </div>
-            </div>
-        </nav>
+                <div className="uppercase font-bold text-sm m-auto mr-0 flex justify-between gap-5 items-center">
+                    <p>{t("contact")}</p>
+                    <Mail mail="kat.hladikova@email.cz" />
+                    <div className="flex justify-center gap-1 text-base">
+                        <Link href={pathname} locale="cs">
+                            CZ
+                        </Link>
+                        |
+                        <Link href={pathname} locale="en">
+                            EN
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        </div>
     );
 }
