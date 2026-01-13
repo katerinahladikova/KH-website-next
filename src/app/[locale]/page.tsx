7f8b60bf0248e5ce7144f819c48455bbd6a6c2c4
katerinahladikova <kat.hladikova@email.cz>
2026-01-13 15:38:28 +0100

    refs tooltip

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -11,18 +11,18 @@ export default async function Home({ params }: Readonly<{ params: Promise<{ loca
             <Hero locale={locale} />
             <h2 dangerouslySetInnerHTML={{ __html: t("header") }} className="h2 container"></h2>
             <div className="container mb-14 grid lg:grid-cols-3 md:grid-cols-2 gap-5 gap-y-10">
-                <Ref url="http://pragostoma-web.dev.effectivelab.cz/" alt="pragostoma" image="/pragostoma.jpg" title={t("t1")} subtitle={t("st1")} badges={["Next.js", "Tailwind", "TypeScript"]} />
-                <Ref url="https://it-centrum.cz/" alt="it centrum" image="/it-centrum.png" title={t("t2")} subtitle={t("st2")} badges={["Next.js", "Tailwind", "TypeScript"]} />
+                <Ref tooltip={t("tooltip")} alt="pragostoma" image="/pragostoma.jpg" title={t("t1")} subtitle={t("st1")} badges={["Next.js", "Tailwind", "TypeScript"]} />
+                <Ref tooltip={t("tooltip")} url="https://it-centrum.cz/" alt="it centrum" image="/it-centrum.png" title={t("t2")} subtitle={t("st2")} badges={["Next.js", "Tailwind", "TypeScript"]} />
+                <Ref tooltip={t("tooltip")} alt="sanstarling" image="/sanstarling.jpg" title={t("t3")} subtitle={t("st3")} badges={["Next.js", "Tailwind", "TypeScript", "Strapi"]} />
                 <Ref
-                    url="http://sanstarling-web.dev.effectivelab.cz/"
-                    alt="sanstarling"
-                    image="/sanstarling.jpg"
-                    title={t("t3")}
-                    subtitle={t("st3")}
-                    badges={["Next.js", "Tailwind", "TypeScript", "Strapi"]}
+                    tooltip={t("tooltip")}
+                    url="https://modulypart66.com/"
+                    alt="moduly part 66"
+                    image="/moduly.jpg"
+                    title={t("t4")}
+                    subtitle={t("st4")}
+                    badges={["Next.js", "Tailwind", "TypeScript"]}
                 />
-
-                <Ref url="https://modulypart66.com/" alt="moduly part 66" image="/moduly.jpg" title={t("t4")} subtitle={t("st4")} badges={["Next.js", "Tailwind", "TypeScript"]} />
             </div>
         </>
     );
--- src/app/components/Ref.tsx
+++ src/app/components/Ref.tsx
@@ -1,19 +1,39 @@
+"use client";
+
 import { Link } from "@/i18n/navigation";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 type RefProps = {
-    url: string;
+    url?: string;
     alt: string;
     image: string;
     title: string;
     subtitle: string;
     badges?: string[];
+    tooltip: string;
 };
 
-export default function Ref({ url, alt, image, title, subtitle, badges }: RefProps) {
+export default function Ref({ url, alt, image, title, subtitle, badges, tooltip }: RefProps) {
+    const Component = url ? Link : "div";
+    const params = url ? { href: url, target: "_blank" } : {};
+    const ref = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        const handleMouseMove = (e: MouseEvent) => {
+            if (ref.current) {
+                ref.current.style.transform = `translateX(${e.clientX + 30}px) translateY(${e.clientY - 10}px)`;
+            }
+        };
+        window.addEventListener("mousemove", handleMouseMove);
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, [ref]);
+
     return (
-        <div>
-            <Link target="_blank" href={url} className="block group rounded-3xl outline-4 outline-offset-4 outline-transparent hover:outline-[#a5a5a58f] transition-colors hover:animate-boom relative">
+        <div className="group">
+            {/* eslint-disable-next-line */}
+            <Component {...(params as any)} className="block group rounded-3xl outline-4 outline-offset-4 outline-transparent hover:outline-[#a5a5a58f] transition-colors hover:animate-boom relative">
                 <div className="absolute top-1/2 left-1/2 -translate-1/2 h-full aspect-square rounded-full bg-[#757575] group-hover:animate-blur blur-[20px] opacity-0 transition-opacity group-hover:opacity-100"></div>
                 <Image src={image} alt={alt} width={1750} height={1200} className="rounded-3xl relative z-10" />
                 <div className="z-20 absolute bottom-4 right-4 left-4 flex flex-wrap gap-2 justify-end">
@@ -25,7 +45,15 @@ export default function Ref({ url, alt, image, title, subtitle, badges }: RefPro
                         );
                     })}
                 </div>
-            </Link>
+            </Component>
+            {!url && (
+                <div
+                    ref={ref}
+                    className="fixed top-0 left-0 opacity-0 transition-opacity group-hover:opacity-100 pointer-events-none shadow-md inline-block bg-[#80091B] py-1 px-2 rounded-2xl text-white text-sm font-light"
+                >
+                    {tooltip}
+                </div>
+            )}
             <h3 className="text-inky text-xl font-semibold pl-2.5 mt-3 mb-1">{title}</h3>
             <p className="text-darkgray text-xs pl-2.5 font-bold">{subtitle}</p>
         </div>
